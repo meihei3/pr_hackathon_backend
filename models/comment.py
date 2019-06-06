ea6387051424ddc346c8fb5f54880f738a12b888
yameholo <ame.aroid.xz5@gmail.com>
2019-06-06 21:05:57 +0900

    bug fix, reformat code

--- models/comment.py
+++ models/comment.py
@@ -1,39 +1,44 @@
 # coding: utf-8
+from db import db
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import pytz
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy(app)
 
 
 # Model
+
+
 class Comment(db.Model):
     __tablename__ = 'comments'
-    id=db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     company_id = db.Column(db.Integer)
-    post_id= db.Column(db.Integer)
+    post_id = db.Column(db.Integer)
     name = db.Column(db.String(80))
-    text=db.Column(db.String())
-    posted_at=db.Column(db.Datetime)
+    text = db.Column(db.String())
+    posted_at = db.Column(db.String())
 
     def to_dict(self):
         return dict(
-            company_id=self.company_id ,
-            post_id=self.post_id ,
-            name=self.name ,
+            company_id=self.company_id,
+            post_id=self.post_id,
+            name=self.name,
             text=self.text,
             poted_at=self.posted_at,
         )
-    def __init__(self, name):
+
+    def __init__(self, name, company_id, post_id, text, posted_at):
         self.name = name
-        self.company_id=company_id
-        self.post_id=post_id
-        self.name=name
-        self.text=text
-        self.posted_at=posted_at
+        self.company_id = company_id
+        self.post_id = post_id
+        self.name = name
+        self.text = text
+        self.posted_at = posted_at
+
     def __repr__(self):
         return '<Model {}>'.format(self.name)
