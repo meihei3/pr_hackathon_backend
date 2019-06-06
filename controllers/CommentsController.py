fd3a5519bb6cb2fe78fe6b7d4fef766244efd4e2
kohge4 <kabukiagenano@gmail.com>
2019-06-06 20:20:58 +0900

    change CommentsController.py

--- controllers/CommentsController.py
+++ controllers/CommentsController.py
@@ -1,20 +1,27 @@
 from flask import request
-# from models.commnets import Comments
+from models.commnets import Comments
 from flask.json import jsonify
 from flask.views import MethodView
-# from db import db
+from db import db
+import datetime
+import pytz
+
 
 
 class CommentController(MethodView):
     def get(self, company_id, post_id):
-        """
-        :return: json
-        """
-        return jsonify({"comments": []})
+        comments=Comment.query.all().filter(post_id=post_id,company_id=company_id)
+        comments=[c.to_dict() for c in comments]
+        return jsonify({"comments": comments})
 
     def post(self, company_id, post_id):
         """
         :return: json
         """
-        text = request.form[""]
+        text = request.form["text"]
+        name=request.form["name"]
+        datetime=datetime.datetime.now(pytz.timezone('Asia/Tokyo')
+        c=Comment(company_id,post_id,name,text,datetime)
+        db.session.add(c)
+        db.session.commit()
         return jsonify({'response': 'ok'})
