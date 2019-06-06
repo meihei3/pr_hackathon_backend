c624862c959fbb9c4bf9cd7333151948ffc4c7ad
yameholo <ame.aroid.xz5@gmail.com>
2019-06-06 16:36:36 +0900

    update main app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from controllers.CommentsController import CommentController
 # import models as db
 
@@ -15,9 +15,9 @@ def create_app():
 
 
 app = create_app()
-
 app.add_url_rule('/comments/<string:company_id>/<string:post_id>',
                  view_func=CommentController.as_view('comment_controller'))
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=777, debug=True, threaded=True)
